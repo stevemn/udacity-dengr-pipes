68fcba5683f1ec0751e224774e9f9b62ad00c6c5
stevemn <steven.mccauley@gmail.com>
2020-11-13 15:53:56 -0500

    Add code review

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -7,7 +7,7 @@ class LoadDimensionOperator(BaseOperator):
     ui_color = '#80BD9E'
 
     DELETE_SQL = """
-        DELETE FROM {}
+        TRUNCATE public.{}
     """
 
     INSERT_SQL = """
@@ -16,19 +16,19 @@ class LoadDimensionOperator(BaseOperator):
     """
 
     @apply_defaults
-    def __init__(self, redshiftConn, table, query, appendData=False,
+    def __init__(self, redshiftConn, table, query, truncate=True,
         *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshiftConn
         self.table = table
-        self.overwrite = not appendData
+        self.truncate = truncate
         self.drop_query = LoadDimensionOperator.DELETE_SQL.format(table)
         self.insert_query = LoadDimensionOperator.INSERT_SQL.format(table, query)
 
     def execute(self, context):
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-        if self.overwrite:
+        if self.truncate:
             self.log.info(self.drop_query)
             redshift.run(self.drop_query)
         self.log.info(self.insert_query)
